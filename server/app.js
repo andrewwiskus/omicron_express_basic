2ffe2f6fdb1912b881e7d7f5bfbfaf8ef598610e
Andrew Wiskus <MyWork@Andrews-MacBook-Pro.local>
2016-08-12 14:51:46 -0500

    woo

--- server/app.js
+++ server/app.js
@@ -7,32 +7,73 @@ var songs = []; //stores our songs
 
 app.set('port', process.env.PORT || 3000);
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 /**
  * POST /songs
  *
  * Places song into songs array
  */
-app.post('/songs', function (req, res) {
-  var song = req.body;
-  songs.push(song);
-  res.sendStatus(200);
+app.post('/songs', function(req, res) {
+
+    var theSong = req.body;
+    var dup = checkDuplicate(theSong.title, theSong.artist);
+    var blank = checkBlank(theSong.title, theSong.artist);
+
+    if (dup === false && blank === false) {
+        songs.push(theSong);
+        res.sendStatus(200);
+    } else {
+        console.log(dup, blank);
+        res.sendStatus(200);
+        console.log("was dup or blank");
+    }
+
+
 });
 
-app.get('/songs', function (req, res) {
-  res.send(songs);
+app.get('/songs', function(req, res) {
+    res.send(songs);
 });
 
-app.get('/*', function (req, res) {
-  var file = req.params[0] || '/views/index.html';
+app.get('/*', function(req, res) {
+    var file = req.params[0] || '/views/index.html';
 
-  console.log('What is in req.params[0]?', req.params[0]);
+    console.log('What is in req.params[0]?', req.params[0]);
 
-  //console.log('dirname: ', __dirname);
-  //console.log('path', path.join(__dirname, '../public', file));
-  res.sendFile(path.join(__dirname, './public', file));
+    //console.log('dirname: ', __dirname);
+    //console.log('path', path.join(__dirname, '../public', file));
+    res.sendFile(path.join(__dirname, './public', file));
 });
 
-app.listen(app.get('port'), function () {
-  console.log('Server now running at port ', app.get('port'));
+app.listen(app.get('port'), function() {
+    console.log('Server now running at port ', app.get('port'));
 });
+
+function checkBlank(title, artist) {
+    if (title === "") {
+        return true;
+    } else if (artist === "") {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+function checkDuplicate(title, artist) {
+    var truth = false;
+    if (songs.length === 0) {
+        truth = false;
+    } else {
+        songs.forEach(function(thisSong, i) {
+            if (thisSong.title == title && thisSong.artist == artist) {
+                truth = true;
+            } else {
+                truth = false;
+            }
+        });
+    }
+    return truth;
+}
--- server/public/scripts/client-app.js
+++ server/public/scripts/client-app.js
@@ -1,50 +1,75 @@
-$(document).ready(function () {
-  getSongs();
+$(document).ready(function() {
+    getSongs();
+    var currentSongs = [];
+    // respond to submit event on form
+    $('#song-form').on('submit', function() {
+        event.preventDefault();
 
-  // respond to submit event on form
-  $('#song-form').on('submit', function () {
-    event.preventDefault();
+        var song = {}; // we will store our song here
+        var duplicate = false;
+        var blank = false;
+        // iterate over form fields
+        // populate our song object with title and artist
+        $.each($('#song-form').serializeArray(), function(i, field) {
+            song[field.name] = field.value;
+            var theDate = new Date();
+            song.dateAdded = theDate;
+            // duplicate = checkDuplicate(song.title, song.artist);
+            // blank = checkBlank(song.title, song.artist);
+        });
 
-    var song = {}; // we will store our song here
+        $.ajax({
+            type: 'POST',
+            url: '/songs',
+            data: song,
 
-    // iterate over form fields
-    // populate our song object with title and artist
-    $.each($('#song-form').serializeArray(), function (i, field) {
-      song[field.name] = field.value;
+            success: function(response) {
+                console.log('POST /songs works!');
+                getSongs();
+            },
+
+            error: function(response) {
+                console.log('Attempted POST /songs, did not work');
+            }
+        });
     });
 
-    // check that the data is getting submitted
-    console.log('song submitted is ', song);
+    // function checkBlank(title, artist) {
+    //     if (title === "") {
+    //         return true;
+    //     } else if (artist === "") {
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // }
+    //
+    // function checkDuplicate(title, artist) {
+    //     currentSongs.forEach(function(song, i) {
+    //         if (song.title == title && song.artist == artist) {
+    //             return true;
+    //         } else {
+    //             return false;
+    //         }
+    //     });
+    // }
 
-    $.ajax({
-      type: 'POST',
-      url: '/songs',
-      data: song,
-      success: function (response) {
-        console.log('POST /songs works!');
-        getSongs();
-      },
+    function getSongs() {
+        $.ajax({
+            type: 'GET',
+            url: '/songs',
+            success: function(songs) {
+                $('#song-list').empty();
+                currentSongs = songs;
+                console.log(currentSongs);
+                songs.forEach(function(song) {
+                    $('#song-list').append('<div>' + song.title + '-' + song.artist + '</div>');
+                });
+            },
 
-      error: function (response) {
-        console.log('Attempted POST /songs, did not work');
-      }
-    });
-  });
-
-function getSongs() {
-  $.ajax({
-    type: 'GET',
-    url: '/songs',
-    success: function (songs) {
-      $('#song-list').empty();
-      songs.forEach(function (song) {
-        $('#song-list').append('<div>' + song.title + '-' + song.artist + '</div>');
-      });
-    },
-
-    error: function () {
-      console.log('GET /songs did not work');
-    },
-  });
-}
+            error: function() {
+                console.log('GET /songs did not work');
+            },
+        });
+    }
 });
